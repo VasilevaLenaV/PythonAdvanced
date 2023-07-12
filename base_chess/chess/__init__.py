e38df59235f9483fd6c9ef48dbe1b7c537896b24
VasilevaLenaV <charoit@icloud.com>
2023-07-12 16:53:47 +0300

    ДЗ к уроку 6

---
+++ base_chess/chess/__init__.py
@@ -0,0 +1,3 @@
+from .arratgement import random_queen_arrangement
+from .chess import queen_will_beat_queen
+from .arratgement import prefill_queens
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from base_chess.chess import queen_will_beat_queen
+from base_chess.chess import random_queen_arrangement
+from base_chess.chess import prefill_queens
+# 1.Добавьте в пакет, созданный на семинаре шахматный модуль. Внутри него напишите код, решающий задачу о 8 ферзях.
+# Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били друг друга.
+# Вам дана расстановка 8 ферзей на доске, определите, есть ли среди них пара бьющих друг друга.
+# Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей. Если ферзи не бьют друг друга верните истину, а если бьют - ложь.
+
+# prefill_auto = prefill_queens()
+# queens_auto = []
+# for x, y in enumerate(prefill_auto):
+#     queens_auto.append((x, y))
+#
+# print(queen_will_beat_queen(queens_auto))
+
+queens = []
+
+for _ in range(8):
+    x, y = map(int, input(f'Введите {_+1} пару чисел от 1 до 8, разделенных пробелом:').split())
+    queens.append((x, y))
+
+print(queen_will_beat_queen(queens))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+# 2.Напишите функцию в шахматный модуль. Используйте генератор случайных чисел для случайной расстановки ферзей в
+# задаче выше. Проверяйте различные случайные варианты и выведите 4 успешных расстановки. *Выведите все успешные
+# варианты расстановок
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+count = 0
+for arrangement in random_queen_arrangement():
+    print(arrangement)
+    count += 1
+    if count == 4:
+        break
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
