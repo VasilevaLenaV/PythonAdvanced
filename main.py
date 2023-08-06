a17eb1ed4398aa451eb90725d9e4ee2838149757
VasilevaLenaV <charoit@icloud.com>
2023-08-06 15:09:44 +0300

    ДЗ к уроку 15

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+# Напишите код, который запускается из командной строки и получает на вход путь до директории на ПК.
+# Соберите информацию о содержимом в виде объектов namedtuple.
+# Каждый объект хранит:
+# * имя файла без расширения или название каталога,
+# * расширение, если это файл,
+# * флаг каталога,
+# * название родительского каталога.
+# В процессе сбора сохраните данные в текстовый файл используя логирование.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
+import logging
+from fileinfo import process_directory, save_to_file
 
+logging.basicConfig(filename='file_info.log', level=logging.INFO)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", help="Путь до директории")
+    args = parser.parse_args()
 
+    directory = args.directory
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    file_info_list, logging = process_directory(directory, logging)
+    save_to_file(file_info_list)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    logging.info("Информацию о содержимом сохранена в файл 'file_info.txt'")
